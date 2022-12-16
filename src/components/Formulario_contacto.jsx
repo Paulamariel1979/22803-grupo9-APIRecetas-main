7ab70d2dc94ba6d4212f805b0788fbfce881d6f2
Paulamariel1979 <101409680+Paulamariel1979@users.noreply.github.com>
2022-12-16 12:26:35 +0100

    formulario

--- src/components/Formulario_contacto.jsx
+++ src/components/Formulario_contacto.jsx
@@ -1,4 +1,58 @@
 import React from 'react'
+import "./Formulario_contacto.css"
+
+import { useForm, ValidationError } from '@formspree/react';
+function ContactForm() {
+    const [state, handleSubmit] = useForm("xgebwegv");
+    if (state.succeeded) {
+        return <p>Te responderemos en 24 horas!</p>;
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email" width="90rem">
+                Correo electrónico
+            </label>
+            <br></br>
+            <br></br>
+            <input
+                id="email"
+                type="email"
+                name="email"
+                width="90rem"
+            />
+            <ValidationError
+                prefix="Email"
+                field="email"
+                errors={state.errors}
+            />
+            <br></br>
+
+            <textarea
+                id="message"
+                name="message"
+            />
+            <ValidationError
+                prefix="Message"
+                field="message"
+                errors={state.errors}
+            />
+            <br></br>
+
+            <button type="submit" disabled={state.submitting}>
+                Enviar
+            </button>
+        </form>
+    );
+}
+function App() {
+    return (
+        <ContactForm />
+    );
+}
+export default App;
+
+{/* import React from 'react'
+
 
 function Formulario_contacto() {
     return (
@@ -7,3 +61,4 @@ function Formulario_contacto() {
 }
 
 export default Formulario_contacto
+*/}
